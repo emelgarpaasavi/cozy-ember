092c436a14c95a56911ff01d8e2f72c9e786d73a
unknown <emelgarpaasa@gmail.com>
2026-01-03 15:00:16 +0800

    add responsive menu and footer layout

--- components/layout/HomeNavigation.jsx
+++ components/layout/HomeNavigation.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "@/public/Cozy ember.svg";
 import cart from "@/public/add_shopping_cart_24px.svg";
+import menu from "@/public/menu_24px.svg";
 import classes from "./HomeNavigation.module.css";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
@@ -10,6 +11,11 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 export default function HomeNavigation({ cartData }) {
   const router = useRouter();
   const [isScrolled, setIsScrolled] = useState(0);
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenu() {
+    setShowMenu((prevSetup) => !prevSetup);
+  }
 
   function handleScroll() {
     const scrollPosition = window.scrollY;
@@ -48,7 +54,7 @@ export default function HomeNavigation({ cartData }) {
           <Link href="/">
             <Image src={logo} alt="Cozy Ember Logo" />
           </Link>
-          <div className={classes.menu}>
+          <div className={classes.web_menu}>
             {router.pathname !== "/" ? (
               <Link href="/">Home</Link>
             ) : (
@@ -59,6 +65,23 @@ export default function HomeNavigation({ cartData }) {
               Cart ({totalItemsInCart})
               <Image src={cart} alt="Cart Icon" />
             </Link>
+          </div>
+          <div className={classes.mobile_menu}>
+            <Image src={menu} alt="Hamburger Menu Icon" onClick={handleMenu} />
+            {showMenu && (
+              <div className={classes.dropdown_menu}>
+                {router.pathname !== "/" ? (
+                  <Link href="/">Home</Link>
+                ) : (
+                  <AnchorLink href="#about">About us</AnchorLink>
+                )}
+                <Link href="/orders">Orders</Link>
+                <Link href="/cart" className={classes.cart}>
+                  Cart ({totalItemsInCart})
+                  <Image src={cart} alt="Cart Icon" />
+                </Link>
+              </div>
+            )}
           </div>
         </nav>
       </header>
